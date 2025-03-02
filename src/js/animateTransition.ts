05908d905795348284e9b9df330d2a41a1885007
Perry Angelora <pea.mail.list@gmail.com>
2025-03-02 13:11:35 -0500

    Bump: WIP

--- src/js/animateTransition.ts
+++ src/js/animateTransition.ts
@@ -1,44 +1,16 @@
 export default function initiateAnimateTransition() {
   const pageWrapper: HTMLDivElement = document.querySelector('.main_wrapper');
+  console.log('MA-NEWER');
 
-  function navigateWithAnimation(href: string, pushState: boolean = true) {
-    if (pageWrapper) {
-
-      pageWrapper.addEventListener('animationend', () => {
-        setTimeout(() => {
-
-          if (pushState) {
-            window.scrollTo({
-              top: 0,
-              left: 0,
-              behavior: "instant"
-            });
-            // history.pushState(null, '', href);
-          }
-          window.location.href = href;
-          pageWrapper.classList.remove('is-interactive');
-        }, 10);
-      }, { once: true });
-
-      pageWrapper.classList.add('fade-out');
-    }
-  }
-
-  // Handle anchor clicks
   document.querySelectorAll('.project-anchor').forEach((link: HTMLAnchorElement) => {
     link.addEventListener('click', (event) => {
       event.preventDefault();
       const href = link.getAttribute('href');
-      if (href) navigateWithAnimation(href);
-    });
-  });
 
-  // Handle back/forward button
-  window.addEventListener('popstate', () => {
-    pageWrapper.classList.add('fade-out');
-    pageWrapper.classList.remove('is-interactive');
-    const href = location.pathname;
-    history.pushState(null, '', href);
-    navigateWithAnimation(href);
+      if (pageWrapper) {
+        pageWrapper.addEventListener('animationend', () => window.location.href = href, { once: true }); // reroute
+        pageWrapper.classList.add('fade-out');
+      }
+    });
   });
 }
